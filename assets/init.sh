b737c0e7f7570f312dd29f82f7aa31293ba6cdf7
Rogerio Lamarques <rogerio.lamarques@gmail.com>
2019-01-31 00:39:00 -0200

    alteração do label mantenedor e teste do init.sh

--- assets/init.sh
+++ assets/init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ ! -f /firstrundone ]; then
-	if [ "$RELAY_HOST" != "" ]; then
-		echo "relayhost = $RELAY_HOST" >> /etc/postfix/main.cf
-	fi
-	if [ "$MASQ_DOMAINS" != "" ]; then
-		echo "masquerade_domains = $MASQ_DOMAINS" >> /etc/postfix/main.cf
-	fi
-	echo "smtp_generic_maps = hash:/etc/postfix/generic" >> /etc/postfix/main.cf
-	echo "root	no-reply@$MASQ_DOMAINS" >> /etc/postfix/generic
-	postmap /etc/postfix/generic
-	echo "done" >> /firstrundone
-fi
+# if [ ! -f /firstrundone ]; then
+# 	if [ "$RELAY_HOST" != "" ]; then
+# 		echo "relayhost = $RELAY_HOST" >> /etc/postfix/main.cf
+# 	fi
+# 	if [ "$MASQ_DOMAINS" != "" ]; then
+# 		echo "masquerade_domains = $MASQ_DOMAINS" >> /etc/postfix/main.cf
+# 	fi
+# 	echo "smtp_generic_maps = hash:/etc/postfix/generic" >> /etc/postfix/main.cf
+# 	echo "root	no-reply@$MASQ_DOMAINS" >> /etc/postfix/generic
+# 	postmap /etc/postfix/generic
+# 	echo "done" >> /firstrundone
+# fi
 
 service syslog start
 service postfix start
